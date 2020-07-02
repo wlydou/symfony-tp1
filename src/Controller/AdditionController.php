45b8b0544eb7712c8377e15dc4c15f65d0c38121
Willy <w.douangpaseut@ecole-ipssi.net>
2020-07-02 21:05:35 +0200

    Added SessionInterface

--- src/Controller/AdditionController.php
+++ src/Controller/AdditionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AdditionController extends AbstractController
 {
@@ -21,7 +22,7 @@ class AdditionController extends AbstractController
      /**
      * @Route("/addition/{xmin}/{xmax}/{ymin}/{ymax} ", name="additionAction")
      */
-    public function additionAction($xmin, $xmax, $ymin, $ymax)
+    public function additionAction($xmin, $xmax, $ymin, $ymax, SessionInterface $session)
     {
         //Vérification des entiers
         if(is_int($xmin) || is_int($xmax) || is_int($ymin) || is_int($ymax)) {
@@ -33,6 +34,12 @@ class AdditionController extends AbstractController
             throw new \Exception('Les valeurs ne sont pas des entiers positifs');
         }
 
+        //Enregistrement des valeurs dans la session
+        $session->set('xmin', $xmin);
+        $session->set('xmax', $xmax);
+        $session->set('ymin', $ymin);
+        $session->set('ymax', $ymax);
+
         return $this->render('addition/index.html.twig', [
             'controller_name' => 'AdditionController',
             'inputs' => [$xmin, $xmax, $ymin, $ymax]
@@ -42,28 +49,35 @@ class AdditionController extends AbstractController
     /**
      * @Route("/addLine", name="addLine")
      */
-    public function addLine(Request $request)
+    public function addLine(Request $request, SessionInterface $session)
     {   
         $inputs =  $request->query->get('inputs');
 
         //Ajout d'une ligne (ymax + 1)
         $inputs[3] += 1;
 
+        //Mise à jour variable session ymax
+        $session->set('ymax', $inputs[3]);
+
         return $this->render('addition/index.html.twig', [
             'controller_name' => 'AdditionController',
-            'inputs' => $inputs
+            'inputs' => $inputs,
+            'xmin' => $param1
         ]);
     }
 
     /**
      * @Route("/addColumn", name="addColumn")
      */
-    public function addColumn(Request $request)
+    public function addColumn(Request $request, SessionInterface $session)
     {   
         $inputs =  $request->query->get('inputs');
 
         //Ajout d'une colonne (xmax + 1)
         $inputs[1] += 1;
+
+        //Mise à jour variable session xmax
+        $session->set('xmax', $inputs[1]);
 
         return $this->render('addition/index.html.twig', [
             'controller_name' => 'AdditionController',
@@ -74,12 +88,15 @@ class AdditionController extends AbstractController
     /**
      * @Route("/removeLine", name="removeLine")
      */
-    public function removeLine(Request $request)
+    public function removeLine(Request $request, SessionInterface $session)
     {   
         $inputs =  $request->query->get('inputs');
 
         //Suppression d'une ligne (ymax - 1)
         $inputs[3] -= 1;
+
+        //Mise à jour variable session ymax
+        $session->set('ymax', $inputs[3]);
 
         return $this->render('addition/index.html.twig', [
             'controller_name' => 'AdditionController',
@@ -90,12 +107,15 @@ class AdditionController extends AbstractController
     /**
      * @Route("/removeColumn", name="removeColumn")
      */
-    public function removeColumn(Request $request)
+    public function removeColumn(Request $request, SessionInterface $session)
     {   
         $inputs =  $request->query->get('inputs');
 
         //Suppression d'une colonne (xmax - 1)
         $inputs[1] -= 1;
+
+        //Mise à jour variable session xmax
+        $session->set('xmax', $inputs[1]);
 
         return $this->render('addition/index.html.twig', [
             'controller_name' => 'AdditionController',
